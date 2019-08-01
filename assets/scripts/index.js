448ffab2c26f2443b55339d98270ff4f8983e1e1
tohearne <thomas.ohearne@yahoo.com>
2019-08-01 11:44:53 -0400

    First pass at portfolio

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,18 @@
 $(() => {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      let target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, 'easeInOutExpo')
+        return false
+      }
+    }
+  })
 
+  $('.js-scroll-trigger').click(() => {
+    $('.navbar-collapse').collapse('hide')
+  })
 })
